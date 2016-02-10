b8f53208ad29c08e6844f2ef888d002bcd9173a2
David Ojeda Lopez <david.ojeda.lopez@gmail.com>
2016-02-09 21:57:38 -0600

    feat: add button to start pomodoro taking the work length value

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 function startPomodoro(){
+
+	$('.pour').removeClass('hidden');
+	$('.fill').removeClass('hidden');
+
+	var breakTime = parseInt($('.break-length-value').html());
+	var workTime = parseInt($('.work-length-value').html());
+
+	$('.fill').css('webkit-animation-duration', workTime * 60 + "s");
+
 	var heading = $('div.pour').clone().removeClass();
     $('div.pour').remove();
     $('div.banner').append(heading);
